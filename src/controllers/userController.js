716c36f7c8c9c4e2c1c3c63b63eeb0ec5fcc099a
guialves50 <guialves.garf50@hotmail.com>
2025-04-12 14:48:11 -0300

    update

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,18 +1,37 @@
 import user from "../models/User.js";
+import bcrpty from "bcrypt";
 
 class UserController {
+  // CRIA UM USUÁRIO
   static async createUser(req, res) {
     try {
-      const userCreated = await user.create(req.body);
+      const userBody = req.body;
+      const salt = await bcrpty.genSalt(10);
+      const hashPassword = await bcrpty.hash(userBody.password, salt);
+
+      await user.create({
+        name: userBody.name,
+        email: userBody.email,
+        phone: userBody.phone,
+        cpf: userBody.cpf,
+        password: hashPassword,
+      });
       res.status(201).json({
         message: "Usuário criado com sucesso",
-        "Novo Usuário": userCreated,
+        "Novo Usuário": {
+          name: userBody.name,
+          email: userBody.email,
+          phone: userBody.phone,
+          cpf: userBody.cpf,
+        },
       });
     } catch (error) {
       res.status(500).json({ message: "Falha ao criar usuário" });
     }
   }
 
+
+  // LISTA OS USUÁRIOS
   static async showUsers(req, res) {
     try {
       const showAllUsers = await user.find({});
@@ -22,6 +41,7 @@ class UserController {
     }
   }
 
+  // MOSTRA UM USUÁRIO PELO ID
   static async showUserById(req, res) {
     try {
       const showFind = await user.findById(req.params.id);
@@ -33,6 +53,7 @@ class UserController {
     }
   }
 
+  // ALTERA DADOS DE UM USUÁRIO PELO ID
   static async updateUserById(req, res) {
     try {
       await user.findByIdAndUpdate(req.params.id, req.body);
@@ -44,12 +65,11 @@ class UserController {
     }
   }
 
+  // REMOVE UM USUÁRIO PELO ID
   static async removeUserById(req, res) {
     try {
-      await user.findByIdAndDelete(req.params.id, req.body)
-      res
-      .status(200)
-      .json({message: "Usuário deletado com sucesso"})
+      await user.findByIdAndDelete(req.params.id);
+      res.status(200).json({ message: "Usuário deletado com sucesso" });
     } catch (error) {
       res.status(500).json({ message: "Falha ao deletar usuário" });
     }
